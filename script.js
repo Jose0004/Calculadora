511d313587e15444c62450bfda64e31a2c500ca6
Jose Carlos Castillo Castillo <jcastilloc28@miumg.edu.gt>
2026-03-03 16:46:43 -0600

    Feature: Agrega funcionalidad de resta

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function sumar() {
   const resultado = n1 + n2;
   document.getElementById("resultado").textContent = "Resultado: " + resultado;
 }
+
+function restar() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+
+  const resultado = n1 - n2;
+  document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
