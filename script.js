ca21e9909a279384577fe88b63b7153f75570bc6
Jose Carlos Castillo Castillo <jcastilloc28@miumg.edu.gt>
2026-03-03 16:58:43 -0600

    Feature: Agrega funcionalidad de division

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ function multiplicar() {
   const resultado = n1 * n2;
   document.getElementById("resultado").textContent = "Resultado: " + resultado;
 }
+
+function dividir() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+
+  if (n2 === 0) {
+    document.getElementById("resultado").textContent = "No se puede dividir entre 0";
+    return;
+  }
+
+  const resultado = n1 / n2;
+  document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
