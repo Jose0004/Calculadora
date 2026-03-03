a362f612821e4556ff4453853aa9527ed3838517
Jose Carlos Castillo Castillo <jcastilloc28@miumg.edu.gt>
2026-03-03 16:40:06 -0600

    Versión inicial calculadora v0.1

---
+++ script.js
@@ -0,0 +1,7 @@
+function sumar() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+
+  const resultado = n1 + n2;
+  document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
