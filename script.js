b61f41d48a8ec3289a8101b65cd0bf59021c7f46
Jose Carlos Castillo Castillo <jcastilloc28@miumg.edu.gt>
2026-03-03 16:51:18 -0600

    Feature: Agrega funcionalidad de multiplicacion

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function restar() {
   const resultado = n1 - n2;
   document.getElementById("resultado").textContent = "Resultado: " + resultado;
 }
+
+function multiplicar() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+
+  const resultado = n1 * n2;
+  document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
